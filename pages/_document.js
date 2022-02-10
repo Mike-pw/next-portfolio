a16a9ecb12628ac4dcc2fa14de9db5c59807fc50
Mike Melgren <mikemelgren@gmail.com>
2022-02-09 22:19:39 -0600

    added icons and logos, robots.txt

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,25 @@ class MyDocument extends Document {
         <Head>
         <title>Portfolio of Mike Melgren</title>
         <meta name="description">Portfolio of Mike Melgren, Web Developer from Canada</meta>
+        <meta charset="utf-8" />
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Portfolio site of Mike Melgren" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta property="og:url" content="https://melgren.dev/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="melgren.dev" />
+        <meta property="og:description" content="Portfolio site of Mike Melgren" />
+        <meta property="og:image" content="https://melgren.dev/thumbnail.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="melgren.dev" />
+        <meta property="twitter:url" content="https://melgren.dev/" />
+        <meta name="twitter:title" content="melgren.dev" />
+        <meta name="twitter:description" content="Portfolio site of Mike Melgren" />
+        <meta name="twitter:image" content="https://melgren.dev/thumbnail.jpg" />
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo.png" />
         </Head>
         <body>
           <Main />
