906995462c634e03ee863069061622e8817e13fc
Mike Melgren <mikemelgren@gmail.com>
2022-02-24 14:47:18 -0600

    changed site meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -26,19 +26,19 @@ class MyDocument extends Document {
         <meta name="description" content="Web Developer from Canada" />
         <meta charset="utf-8" />
         <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Portfolio site of Mike Melgren" />
+        <meta name="description" content="melgren.dev" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
         <meta property="og:url" content="https://melgren.dev/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="melgren.dev" />
-        <meta property="og:description" content="Portfolio site of Mike Melgren" />
+        <meta property="og:description" content="melgren.dev" />
         <meta property="og:image" content="https://melgren.dev/thumbnail.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="melgren.dev" />
         <meta property="twitter:url" content="https://melgren.dev/" />
         <meta name="twitter:title" content="melgren.dev" />
-        <meta name="twitter:description" content="Portfolio site of Mike Melgren" />
+        <meta name="twitter:description" content="melgren.dev" />
         <meta name="twitter:image" content="https://melgren.dev/thumbnail.jpg" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/logo.png" />
