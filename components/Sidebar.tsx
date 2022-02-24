410facbf99b825d8dd3d30271dbb23717c6b4194
Mike Melgren <mikemelgren@gmail.com>
2022-02-24 14:53:07 -0600

    changed sidebar title

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -71,7 +71,7 @@ const SidebarContent = ({ LinkItems, selectFilter, onClose, ...rest }: SidebarPr
             {...rest}>
             <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
                 <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-                    Skills
+                    Tech
                 </Text>
                 <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
             </Flex>
