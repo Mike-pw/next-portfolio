8048fdac465bde2233ccae75540d71e3471f634a
Mike Melgren <mikemelgren@gmail.com>
2022-05-16 15:03:41 -0500

    minor wording comment

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,8 @@ import '../styles/blogPost.css'
 import '../styles/prism-theme.css'
 import '@fontsource/pathway-gothic-one/400.css'
 
-//focus outline - a11y
+//focus outline - acessibility
+
 const focusShadow = '0 0 0 2px rgba(125,125,125,.75)'
 
 const theme = extendTheme({
